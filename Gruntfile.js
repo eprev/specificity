33b4b6ae8980690fe84f0006608bd6a37285bebe
Anton Eprev <a.eprev@gmail.com>
2014-12-06 15:54:13 +0100

    Dotfiles, Gruntfile and specs

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.config.init({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            test : {
+                files: ['src/**/*.js', 'spec/**/*.js'],
+                tasks: ['test']
+            }
+        },
+
+        clean: {
+            tests: ['spec/tmp', 'coverage']
+        },
+
+        jasmine_node: {
+            src: {
+                options: {
+                    specs: './spec'
+                }
+            }
+        },
+
+        istanbul: {
+            src: {
+                options: {
+                    specs: './spec'
+                }
+            }
+        },
+
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'src/**/*.js',
+                'spec/**/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
+
+    grunt.registerMultiTask('jasmine_node', 'Run jasmine-node', function () {
+
+        var options = this.options(),
+            done = this.async();
+
+        grunt.util.spawn({
+            cmd: './node_modules/.bin/jasmine-node',
+            args: [options.specs]
+        }, function (error, result) {
+            if (error) {
+                grunt.log.error(error);
+                grunt.log.write(result.stdout);
+                done(false);
+            } else {
+                grunt.log.write(result.stdout);
+                done();
+            }
+        });
+
+    });
+
+    grunt.registerMultiTask('istanbul', 'Run istanbul cover', function () {
+
+        var options = this.options(),
+            done = this.async();
+
+        grunt.util.spawn({
+            cmd: './node_modules/.bin/istanbul',
+            args: [
+                'cover',
+                '--root', './src',
+                '--verbose',
+                '--hook-run-in-context',
+                '--report', 'html',
+                './node_modules/.bin/jasmine-node', '--', options.specs
+            ]
+        }, function (error, result) {
+            if (error) {
+                grunt.log.error(error);
+                grunt.log.write(result.stdout);
+                done(false);
+            } else {
+                grunt.log.write(result.stdout);
+                done();
+            }
+        });
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('test', ['clean', 'jshint', 'jasmine_node']);
+    grunt.registerTask('coverage', ['clean', 'istanbul']);
+
+    grunt.registerTask('default', ['test']);
+
+};
---
+++ spec/utils.js
@@ -0,0 +1,43 @@
+var diff = require('diff');
+
+exports.V = require('../lib/v');
+
+exports.setupMatchers = function () {
+
+    this.addMatchers({
+        toEqualJSON: function (expected) {
+
+            var actualString = typeof this.actual === 'string' ? this.actual : JSON.stringify(this.actual),
+                expectedString = typeof expected === 'string' ? expected : JSON.stringify(expected);
+
+            this.message = function () {
+                var msg = '';
+                diff.diffChars(actualString, expectedString).forEach(function (part) {
+                    var color = part.added ? '\033[42m' : part.removed ? '\033[41m' : '\033[1;30m';
+                    msg += color + part.value + '\033[0m';
+                });
+                return msg;
+            };
+
+            return actualString === expectedString;
+
+        },
+        toThrowMatch: function (expected) {
+            var actual = this.actual,
+                notText = this.isNot ? ' not' : '';
+            try {
+                actual();
+            } catch (e) {
+                this.message = function () {
+                    return 'Expected function' + notText + ' to throw ' + expected + ', but it threw ' + e;
+                };
+                return expected.test(e);
+            }
+            this.message = function () {
+                return 'Expected function' + notText + ' to throw ' + expected;
+            };
+            return false;
+        }
+    });
+
+};
