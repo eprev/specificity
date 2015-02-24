ebd9edbe08260b650d10a193b01ef825cb11ad14
Anton Eprev <a.eprev@gmail.com>
2015-02-24 19:19:20 +0100

    Added --no-color option #15

--- src/reports/json.js
+++ src/reports/json.js
@@ -2,10 +2,10 @@
 
 var inspect = require('util').inspect;
 
-module.exports = function (data) {
+module.exports = function (data, commandOptions) {
     data.forEach(function (data) {
         console.log('File:', data.file);
-        console.log(inspect(data.json, { depth: null, colors: true }));
+        console.log(inspect(data.json, { depth: null, colors: commandOptions.color }));
     });
 };
 
